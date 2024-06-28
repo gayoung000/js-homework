f630a48b04fec8730514b01645e4ab2e7d518cf0
gayoung000 <167841723+gayoung000@users.noreply.github.com>
2024-06-28 16:22:29 +0900

    Refactoring mission02 - 함수 재사용성 고려 및 리팩토링

--- mission02/poster/client/js/main.js
+++ mission02/poster/client/js/main.js
@@ -10,6 +10,7 @@
 
 */
 
+// DOM 요소 객체 접근
 const nav = document.querySelector('nav');
 const mainImg = document.querySelector('.visual img');
 const h1 = document.querySelector('h1');
@@ -28,78 +29,57 @@ const audioSources = [
 // 전역 Audio 객체
 let currentAudio = null;
 
-// ************ 이미지 set 함수 ************ //
-
-function setImage(e){
-
-  // target의 li 수집
-  const targetLi = e.target.closest('li');
-
-  // li 없는 ul 클릭 시 이벤트 종료
-  if(!targetLi) return;
-
-  // targetLi에 해당하는 index 값 가져오기
-  const targetIndex = targetLi.dataset.index;
-
-  // 위의 index에 해당하는 data의 name 속성 가져오기
-  const targetDataName = data[targetIndex-1].name; // .toLowerCase() 안해도 됨.
-
-  // is-active 속성 넣고 빼기 
-  const children = ul.children;
-  for (let targetLi of children) {
-    targetLi.classList.remove("is-active");
-  }
-  targetLi.classList.add("is-active");
 
 
-  // mainImg의 이미지 소스와 대체 텍스트 변경
-  mainImg.src = `./assets/${targetDataName}.jpeg`;
-  mainImg.alt = data[targetIndex-1].alt;  
+// =============================================== //
+//                    유틸 함수                      //
+// =============================================== //
+
+function isString(data){
+  return Object.prototype.toString.call(data).slice(8,-1).toLowerCase()  === 'string';
 }
 
 
 
-// ************ 상단 텍스트 set 함수 ************ //
+// =============================================== //
+//                    렌더링 함수                     //
+// =============================================== //
 
-function setNameText(e){
- const targetLi = e.target.closest('li');
- if(!targetLi) return;
+// ========= 이미지 set 함수 ======== //
 
- const targetIndex = targetLi.dataset.index;
- const targetDataName = data[targetIndex-1].name; 
-
- h1.textContent = targetDataName;
-}
-
-
-
-// ************ 배경 색상 set 함수 ************ //
-
-function setBgColor(e){
-  const targetLi = e.target.closest('li');
-  if(!targetLi) return;
- 
-  const targetIndex = targetLi.dataset.index;
-  const colorA = data[targetIndex-1].color[0];
-  const colorB = data[targetIndex-1].color[1] || '#000';
-
-  body.style.background = `linear-gradient(to bottom, ${colorA},${colorB})`;
-}
-
-
-
-
-
-// ************ 오디오 set 함수 ************ //
-
-function setAudio(e) {
-  const targetLi = e.target.closest('li');
-  if (!targetLi) return;
-
-  const targetIndex = targetLi.dataset.index;
-  const targetDataSrc = audioSources[targetIndex-1].src;
+function setImage(node, {src, alt}){
+  if (isString(node)) node = document.querySelector(node);
   
-  if (!targetDataSrc) return;
+  // node의 이미지 변경
+  node.src = src;
+  node.alt = alt; 
+}
+
+
+// ========= 상단 텍스트 set 함수 ======== //
+
+function setNameText(node, name){
+  if (isString(node)) node = document.querySelector(node);
+
+  // node의 내용 변경
+  node.textContent = name;
+}
+
+
+// ========= 배경 색상 set 함수 ======== //
+
+function setBgColor(node, colorA, colorB='#000'){
+  if (isString(node)) node = document.querySelector(node);
+
+  // node의 색상 변경
+  node.style.background = `linear-gradient(to bottom, ${colorA},${colorB})`;
+}
+
+
+// ========= 오디오 set 함수 ======== //
+
+function setAudio(src) {
+  if (!src) return;
 
   // 기존 오디오 재생 중이면 멈춤.
   if (currentAudio) {
@@ -107,17 +87,60 @@ function setAudio(e) {
   }
 
   // 새로운 오디오 재생
-  currentAudio = new AudioPlayer(targetDataSrc);
+  currentAudio = new AudioPlayer(src);
   currentAudio.play();
 }
 
 
 
+// =============================================== //
+//                  이벤트 헨들링 함수                  //
+// =============================================== //
+function handleNavClick (e){
 
-nav.addEventListener('click', setImage)
-nav.addEventListener('click', setNameText)
-nav.addEventListener('click', setBgColor)
-nav.addEventListener('click', setAudio);
+  // target 
+  const target = e.target
+
+  // target의 li 수집
+  const li = target.closest('li');
+
+  // li 없는 ul 클릭 시 이벤트 종료
+  if(!li) return;
+
+  // li에 해당하는 index 값 가져오기
+  const index = li.dataset.index;
+
+  // 위의 index에 해당하는 data의 name 속성 가져오기
+  const dataName = data[index-1].name; 
+
+  // is-active 속성 넣고 빼기 
+  const children = ul.children;
+  for (let targetLi of children) {
+    targetLi.classList.remove("is-active");
+  }
+  li.classList.add("is-active");
+  
+  // 렌더링 함수에서 필요할 변수
+  const mainImgData = {
+    src : `./assets/${dataName}.jpeg`,
+    alt :  data[index-1].alt
+  }
+
+  const backgroundColor = {
+    colorA : data[index-1].color[0],
+    colorB : data[index-1].color[1] 
+  }
+
+  const dataSrc = audioSources[index-1].src;
+
+  // 렌더링 함수 호출하기
+  setImage(mainImg, mainImgData);
+  setNameText(h1, dataName);
+  setBgColor(body, backgroundColor);
+  setAudio(dataSrc);
+}
+
+nav.addEventListener('click', handleNavClick)
 
 
 
