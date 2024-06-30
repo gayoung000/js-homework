db468b4e97f7fe2a053ddd16dcfcc61bbf929412
gayoung000 <167841723+gayoung000@users.noreply.github.com>
2024-07-01 00:49:30 +0900

    Finish mission03

--- mission03/client/main.js
+++ mission03/client/main.js
@@ -1,6 +1,37 @@
 // Initialize Swiper 
 
-// 모든 슬라이드 요소를 가져오기
+// 렌덤 숫자 만들기
+// const random = (Math.floor(Math.random() * 6)) +1 ;
+
+
+
+const usedNumbers = new Set();
+
+function getRandomNumber() {
+  let number;
+  do {
+    number = (Math.floor(Math.random() * 6)) + 1;
+  } while (usedNumbers.has(number));
+    usedNumbers.add(number);
+    return number;
+}
+
+
+
+// swiper-slide 요소에 클릭 이벤트 추가
+const slides = document.querySelectorAll('.swiper-slide');
+
+slides.forEach(slide => {
+  const img = slide.querySelector('img');
+  // img.src = `./assets/back${random}.png`;
+  img.src = `./assets/back${getRandomNumber()}.png`;
+  slide.addEventListener('click', () => {
+    slide.classList.toggle('flipped');
+  });
+});
+
+
+
 
 new Swiper(".mySwiper", {
   // 모바일 버전
@@ -12,68 +43,24 @@ new Swiper(".mySwiper", {
     clickable: true,
   },
   // 아래가 안먹음
-  breakpoints: {
-        
-    480: {
-      slidesPerView: 'auto',  //브라우저가 480보다 클 때
-      spaceBetween: 30,
-    },
+  // breakpoints: {
+  //   480: {
+  //     slidesPerView: 'auto',  //브라우저가 480보다 클 때
+  //     spaceBetween: 30,
+  //   },
 
-    768: {
-      slidesPerView: 'auto',  //브라우저가 768보다 클 때
-      spaceBetween: 30,
-    },
-  },
+  //   768: {
+  //     slidesPerView: 'auto',  //브라우저가 768보다 클 때
+  //     spaceBetween: 30,
+  //   },
+  // },
 });
 
-
-// swiper-slide 요소에 클릭 이벤트 추가
-const slides = document.querySelectorAll('.swiper-slide');
-slides.forEach(slide => {
-  slide.addEventListener('click', () => {
-    slide.classList.toggle('flipped');
-  });
-});
 
 
 const refreshButton = document.getElementById('refreshButton');
 refreshButton.addEventListener('click', () => {
   location.reload();
 });
-
-
-
-/*document.addEventListener("DOMContentLoaded", function() {
-  const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 40,
-      },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 50,
-      },
-    },
-  });
-
-  // swiper-slide 요소에 클릭 이벤트 추가
-  const slides = document.querySelectorAll('.swiper-slide');
-  slides.forEach(slide => {
-    slide.addEventListener('click', () => {
-      slide.classList.toggle('flipped');
-    });
-  });
-});
-*/
-
-
-
 
 
